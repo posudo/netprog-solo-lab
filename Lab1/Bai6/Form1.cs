bac6e7b705f0b158a50eddd5a678d88ebb0792e5
posudo <mmttriet@gmail.com>
2024-09-21 14:31:16 +0700

    Thêm control vào form bài 6

--- Lab1/Bai6/Form1.cs
+++ Lab1/Bai6/Form1.cs
@@ -16,5 +16,15 @@ namespace Bai6
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
