8d6f90fb9d9daa04442b8f3b679720ee3574fee1
posudo <mmttriet@gmail.com>
2024-09-24 19:41:43 +0700

    Thêm chức năng cho button "Thêm" và button "Xóa"

--- Lab1/Bai9/Bai9Form.cs
+++ Lab1/Bai9/Bai9Form.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,12 +13,29 @@ namespace Bai9
 {
     public partial class Bai9Form : Form
     {
-        List<string> Foods = new List<string> { "Bún riêu cua","Bún bò","Bún","Phở","Cơm","Tôm","Lê","Không ăn gì hết!" };
+        private List<string> Foods { get; set;}
         public Bai9Form()
         {
             InitializeComponent();
+            LoadFoodsList();
         }
-
+        private void LoadFoodsList()
+        {
+            if(File.Exists("foods.txt"))
+            {
+                Foods = File.ReadAllLines("foods.txt").ToList();
+            }    
+            else
+            {
+                Foods = new List<string> { "Bún riêu cua", "Bún bò", "Bún", "Phở", "Cơm", "Tôm", "Lê", "Không ăn gì hết!"};
+                SaveFoodsList();
+            }
+            listBox1.DataSource = new BindingSource(Foods, null);
+        }
+        private void SaveFoodsList()
+        {
+            File.WriteAllLines("foods.txt", Foods);
+        }
         private void button2_Click(object sender, EventArgs e)
         {
 
@@ -26,10 +44,14 @@ namespace Bai9
         private void button1_Click(object sender, EventArgs e)
         {
             Foods.Add(textBox1.Text);
+            SaveFoodsList();
+            ((BindingSource)listBox1.DataSource).ResetBindings(false);
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
+           
+              
 
         }
 
@@ -41,11 +63,13 @@ namespace Bai9
         private void button3_Click(object sender, EventArgs e)
         {
             Foods.Remove(listBox1.SelectedItem.ToString());
+            SaveFoodsList();
+            ((BindingSource)listBox1.DataSource).ResetBindings(false);
         }
 
         private void Bai9Form_Load(object sender, EventArgs e)
         {
-            listBox1.DataSource = new BindingSource(Foods, null);
+            
         }
     }
 }
