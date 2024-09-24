c45ccfc78c1ad0e9b64aa1725446f360b3aa4619
posudo <mmttriet@gmail.com>
2024-09-24 21:07:04 +0700

    Thêm control bài 8

--- Lab1/Bai8/Bai8Form.Designer.cs
+++ Lab1/Bai8/Bai8Form.Designer.cs
@@ -33,6 +33,7 @@
             this.button2 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -48,9 +49,9 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(390, 74);
+            this.button1.Location = new System.Drawing.Point(408, 69);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(55, 175);
+            this.button1.Size = new System.Drawing.Size(55, 60);
             this.button1.TabIndex = 1;
             this.button1.Text = "Thực hiện!";
             this.button1.UseVisualStyleBackColor = true;
@@ -58,22 +59,25 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(469, 74);
+            this.button2.Location = new System.Drawing.Point(469, 69);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(55, 175);
+            this.button2.Size = new System.Drawing.Size(55, 60);
             this.button2.TabIndex = 2;
             this.button2.Text = "Xóa";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 48);
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 37);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(512, 20);
+            this.textBox1.Size = new System.Drawing.Size(512, 26);
             this.textBox1.TabIndex = 3;
             // 
             // comboBox1
             // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] {
             "Xem thông tin sinh viên",
@@ -81,16 +85,26 @@
             "Tìm môn điểm cao nhất, thấp nhất",
             "Tìm số môn đậu, không đậu",
             "Xếp loại sinh viên "});
-            this.comboBox1.Location = new System.Drawing.Point(12, 86);
+            this.comboBox1.Location = new System.Drawing.Point(12, 69);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(372, 21);
+            this.comboBox1.Size = new System.Drawing.Size(390, 28);
             this.comboBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(12, 103);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(390, 26);
+            this.textBox2.TabIndex = 5;
             // 
             // Bai8Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(536, 557);
+            this.ClientSize = new System.Drawing.Size(536, 141);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
@@ -110,6 +124,7 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
