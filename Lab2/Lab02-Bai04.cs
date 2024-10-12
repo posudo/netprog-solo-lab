851ad94cc268d6766fb2b71dc4d9cd5f9241059a
posudo <mmttriet@gmail.com>
2024-10-12 20:48:19 +0700

    Thêm form bài 4

---
+++ Lab2/Lab02-Bai04.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab2
+{
+    public partial class Lab02_Bai04 : Form
+    {
+        private List<Student> students = new List<Student>();
+        public class Student
+        {
+            public string FullName { get; set; }
+            public int MSSV { get; set; }
+            public string PhoneNumber { get; set; }
+            public float Mark1 { get; set; }
+            public float Mark2 { get; set; }
+            public float Mark3 { get; set; }
+            public float AverageMark { get; set; }
+        }
+        public Lab02_Bai04()
+        {
+            InitializeComponent();
+        }
+
+        private void Lab02_Bai04_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+
+
+
+
+
+        }
+    }
+}
