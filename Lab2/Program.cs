65886d6d643d961dac8e34e2e5a3238ed1245a5a
posudo <mmttriet@gmail.com>
2024-10-14 01:20:28 +0700

    Ignore .vs, commit all remaining changes

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -16,7 +16,7 @@ namespace Lab2
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Lab02_Bai03());
+            Application.Run(new Lab02_Bai07());
         }
     }
 }
