2245b9f998a343dedccf7217c5cf383b62af116f
posudo <mmttriet@gmail.com>
2024-10-13 15:22:09 +0700

    Tạo form bài 7 và thêm các chức năng cho button

---
+++ Lab2/Lab02-Bai07.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Lab2
+{
+    partial class Lab02_Bai07
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 12);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(191, 498);
+            this.treeView1.TabIndex = 2;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(209, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(587, 498);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "File Content";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(6, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(575, 473);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox1.Location = new System.Drawing.Point(6, 19);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(575, 473);
+            this.textBox1.TabIndex = 8;
+            // 
+            // Lab02_Bai07
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(808, 522);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Lab02_Bai07";
+            this.Text = "Lab02_Bai07";
+            this.Load += new System.EventHandler(this.Lab02_Bai07_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ Lab2/Lab02-Bai07.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Lab2
+{
+    public partial class Lab02_Bai07 : Form
+    {
+        public Lab02_Bai07()
+        {
+            InitializeComponent();
+            LoadDirectoryTree();
+            treeView1.BeforeExpand += treeView1_BeforeExpand;
+        }
+
+        private void Lab02_Bai07_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoadDirectoryTree()
+        {
+            TreeNode rootNode;
+
+            foreach (string drive in Directory.GetLogicalDrives())
+            {
+                rootNode = new TreeNode(drive);
+                rootNode.Text = drive; 
+                rootNode.Tag = drive;
+                rootNode.Nodes.Add("...");
+                treeView1.Nodes.Add(rootNode);
+            }
+        }
+        private void treeView1_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            if (e.Node.Nodes[0].Text == "..." && e.Node.Nodes.Count == 1)
+            {
+                e.Node.Nodes.Clear();
+
+                try
+                {
+                    string path = e.Node.Tag.ToString();
+                    if (Directory.Exists(path))
+                    {
+                        string[] dirs = Directory.GetDirectories(path);
+                        foreach (string dir in dirs)
+                        {
+                            TreeNode dirNode = new TreeNode(Path.GetFileName(dir));
+                            dirNode.Tag = dir;
+                            dirNode.Nodes.Add("...");
+                            e.Node.Nodes.Add(dirNode);
+                        }
+
+                        string[] files = Directory.GetFiles(path);
+                        foreach (string file in files)
+                        {
+                            TreeNode fileNode = new TreeNode(Path.GetFileName(file));
+                            fileNode.Tag = file;
+                            e.Node.Nodes.Add(fileNode);
+                        }
+                    }
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    MessageBox.Show("Bạn không có quyền truy cập file này (-_-') ");
+                }
+
+                e.Node.Expand();
+            }
+        }
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            TreeNode selectedNode = e.Node;
+            string path = selectedNode.Tag.ToString();
+
+            if (File.Exists(path)) 
+            {
+                string extension = Path.GetExtension(path).ToLower();
+
+                if (extension == ".txt")
+                {
+                   
+                    textBox1.Text = File.ReadAllText(path);
+                    pictureBox1.Image = null;
+                    pictureBox1.Visible = false;
+                }
+                else if (extension == ".jpg" || extension == ".png" || extension == ".bmp"|| extension == ".gif")
+                {
+                    pictureBox1.Image = Image.FromFile(path);
+                    textBox1.Clear();
+                    pictureBox1.Visible = true;
+                }
+                else
+                {
+                    textBox1.Text = "Không xem được file này (-_-')";
+                    pictureBox1.Visible = false;
+                }
+            }
+        }
+    }
+}
