47edcb592a542ec77330a3b539eed48f9b3b02a1
Posu <112594221+posudo@users.noreply.github.com>
2025-02-17 22:02:23 +0700

    Add exercise description and SQL querry

---
+++ SQLQuery1.sql
@@ -0,0 +1,13 @@
+﻿CREATE DATABASE MyDatabase
+USE MyDatabase
+
+CREATE TABLE Users (
+    UserId INT PRIMARY KEY IDENTITY(1,1),
+    Username NVARCHAR(50) NOT NULL,
+    Password NVARCHAR(256) NOT NULL, 
+    Email NVARCHAR(100) NOT NULL
+)
+/*Nếu password quá dài cột sẽ không chứa được, lúc đó nên đổi kiểu NVCHAR qua VARBINARY
+Đổi kiếu string qua byte[] cho hashed password*/
+
+
